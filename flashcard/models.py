e0c4ab3cad8d0b39be5e883966b9171ffa7e93fd
StephenTurk90 <stephenjturk@gmail.com>
2022-08-25 18:17:57 -0400

    had a critical error started from scratch

--- flashcard/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import AbstractUser as BaseUser
-from django.db import models
-
-# Create your models here.
-class User(BaseUser):
-    # could add custom user attributes here
-    pass
