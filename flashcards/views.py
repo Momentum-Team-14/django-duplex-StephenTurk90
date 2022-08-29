ec4c2477d41420f005ab250bf10e4452406f742c
StephenTurk90 <stephenjturk@gmail.com>
2022-08-29 12:57:16 -0400

    got to the point to create user

--- flashcards/views.py
+++ flashcards/views.py
@@ -3,6 +3,6 @@ from django.utils import timezone
 from .models import Card
 
 
-def post_list(request):
-    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
-    return render(request, 'post_list.html', {})
+def card_list(request):
+    cards = Card.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    return render(request, 'flashcards/card_list.html', {'cards':cards})
