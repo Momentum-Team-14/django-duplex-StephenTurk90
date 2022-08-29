5d399de37fea1104145715dd7521e1be83661bba
StephenTurk90 <stephenjturk@gmail.com>
2022-08-29 15:07:08 -0400

    start from scratch, kept .gitignore file

--- flashcards/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.card_list, name='card_list'),
-]
--- flashcards/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.utils import timezone
-from .models import Card
-
-
-def card_list(request):
-    cards = Card.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
-    return render(request, 'flashcards/card_list.html', {'cards':cards})
