e43cbe5820033df8a5685408d384877d8514c65b
StephenTurk90 <stephenjturk@gmail.com>
2022-08-31 11:45:12 -0400

    got flashcards to appear on the homepage

--- flashcards/views.py
+++ flashcards/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Flashcards
 
 def flashcards(request):
+    flashcards = Flashcards.objects.all()
     return render(request, 'flashcards/flashcards.html', {'flashcards': flashcards})
