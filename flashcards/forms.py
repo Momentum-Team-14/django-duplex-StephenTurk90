1532712a6b4e0a9b774e58f2ed0731f9430d3328
StephenTurk90 <stephenjturk@gmail.com>
2022-09-01 10:44:27 -0400

    added a deck to models and made code to establish ways to create, delete, and edit a deck.

--- flashcards/forms.py
+++ flashcards/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from .models import Flashcards
+from .models import Flashcards, Deck
 
 
 class FlashcardsForm(forms.ModelForm):
     class Meta:
         model = Flashcards
         fields = ('title', 'question', 'answer')
+
+class DeckForm(forms.ModelForm):
+    class Meta:
+        model = Deck
+        fields = ('category',)
--- flashcards/views.py
+++ flashcards/views.py
@@ -1,6 +1,47 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Flashcards
-from .forms import FlashcardsForm
+from .models import Flashcards, Deck
+from .forms import FlashcardsForm, DeckForm
+from django.shortcuts import redirect
+
+
+def deck(request):
+    deck = Deck.objects.all()
+    return render(request, 'flashcards/list_decks.html'), {'decks': decks}
+
+
+def new_deck(request):
+    if request.method == 'POST':
+        form = DeckForm(request.POST)
+        if form.is_valid():
+            deck = form.save()
+            return redirect('results', pk=deck.pk)
+    else:
+        form = DeckForm()
+    return render(request, 'flashcards/new_deck.html', {'form': form})
+
+
+def deck_detail(request, pk):
+    deck = get_object_or_404(Deck, pk=pk)
+    cards = deck.cards.all()
+    return render(request, 'flashcards/deck_detail.html', {'deck': deck})
+
+
+def edit_deck(request, pk):
+    deck = get_object_or_404(Deck, pk=pk)
+    if request.method == 'POST':
+        deck_form = DeckForm(request.POST, instance=deck)
+        if deck_form.is_valid():
+            deck = deck_form.save()
+            return redirect('deck_detail', pk=deck.pk)
+    else:
+        deck_form = DeckForm(instance=deck)
+    return render(request, 'flashcards/edit_deck.html', {'form': form})
+
+
+def delete_deck(request, pk):
+    deck = get_object_or_404(Deck, pk=pk)
+    deck.delete()
+    return redirect('flashcards')
 
 
 def flashcards(request):
@@ -23,6 +64,7 @@ def create_card(request):
         form = FlashcardsForm()
     return render(request, 'flashcards/create_card.html', {'form': form})
 
+
 def edit_card(request, pk):
     flashcard = get_object_or_404(Flashcards, pk=pk)
     if request.method == 'POST':
@@ -35,7 +77,8 @@ def edit_card(request, pk):
         form = FlashcardsForm(instance=flashcard)
     return render(request, 'flashcards/edit_card.html', {'form': form})
 
-def delete(request, pk):
+
+def delete_flashcard(request, pk):
     flashcard = get_object_or_404(Flashcards, pk=pk)
     flashcard.delete()
     return redirect('flashcards')
